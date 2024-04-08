60243f9c539cb17ce50bf29886948db132f92744
Medovshchuk Lera <leramed963@gmail.com>
2024-04-08 17:44:08 +0300

    ogin from videj

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "2589630147",
+  database: "supermarket",
+});
+app.post("/supermarket", (req, res) => {
+  const sql = "INSERT INTO login(`name`,`email`, `password`) VALUES(?)";
+  const values = [req.body.name, req.body.email, req.body.password];
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json(data);
+  });
+});
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    if (data.length > 0) {
+      return res.json("Success");
+    } else {
+      return res.json("Faile");
+    }
+  });
+});
+app.listen(8081, () => {
+  console.log("listening");
+});
